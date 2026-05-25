5e511a5897432cb227ad152e441ad95189fd70e8
Christopher Locricchio <christopher.locricchio@gm.com>
2026-05-25 12:03:49 -0400

    Final functional version

--- module_1/app.py
+++ module_1/app.py
@@ -3,4 +3,4 @@ from personal_website_app import create_app, run_app
 app = create_app()
 
 if __name__ == "__main__":
-    run_app(app, host="127.0.0.1", port=8080)
+    run_app(app, host="0.0.0.0", port=8080)
--- module_1/personal_website_app/pages/pages.py
+++ module_1/personal_website_app/pages/pages.py
@@ -4,23 +4,12 @@ bp = Blueprint('pages', __name__)
 
 @bp.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('index.html')
 
 @bp.route('/contacts')
 def contacts():
-    return """<h2>Contact Information:</h2>
-    <p>Email: clocricchio@gmail.com</p>
-    <p><a href="https://www.linkedin.com/in/christopher-locricchio" target="_blank">LinkedIn</a></p>
-    <p><a href="https://github.com/cmlocric" target="_blank">github.com/cmlocric</a></p>
-    <p><a href="/" class="btn btn-primary">Back to Home</a></p>
-    """
+     return render_template('contacts.html')
 
 @bp.route('/publications')
 def publications():
-    return """<h2>Publications:</h2>
-    <p>Module 1: Personal Website App</p>
-    <p>My first personal developer website that includes a biography, contact information/links, 
-        and details about current and future Python projects.</p>
-    <p><a href="https://github.com/cmlocric/jhu_software_concepts/tree/main/module_1" target="_blank">module_1_repository</a></p>
-    <p><a href="/" class="btn btn-primary">Back to Home</a></p>
-    """
+      return render_template('publications.html')
